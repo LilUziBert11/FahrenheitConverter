bc2af2f8291fcae23d5cacfaf88d5ffaf6f4f6a1
albert <albert.duenas22@gmail.com>
2022-09-26 18:34:33 -0700

    Final project Added Input Validation and used prompt

--- src/FahrenheitConverter.java
+++ src/FahrenheitConverter.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
+
 public class FahrenheitConverter {
 
-    public static void main(String[] args) {
-        CelsiusToFahrenheit(60);
-        double celsiusValue = CelsiusToFahrenheit(60);
-        System.out.format("60 Fahrenheit is %.2f Celsius",celsiusValue);
-
-    }
-
-    private static double CelsiusToFahrenheit(float celsius){
+    private static double CelsiusToFahrenheit(float celsius){ //first method
 
         double fahrenheitValue;
         fahrenheitValue= (1.8)*celsius + 32;
         return fahrenheitValue;
     }
+    private static boolean InputValidation(float input){ //second method + input validation
 
+        if (input < -30){
+            return false;
+        }
+        if (input > 200){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input your celsius degree between -30 and 200"); //Ask user for C degree between specific numbers
+        float userInput = sc.nextFloat(); //save C degree
+        if (InputValidation(userInput)){ //Validate C degree
+            double celsiusValue = CelsiusToFahrenheit(userInput); //passed the C degree (userInput) to the method
+            System.out.format("%.2f Fahrenheit is %.2f Celsius",userInput, celsiusValue); //displays the results based of the users input
+        }
+        else {
+            System.out.println("Please enter a valid number");
+        }
+
+    }
 }
+
